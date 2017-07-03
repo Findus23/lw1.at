82016ce6c8cc9b988e2ab1218b8dd239b8ff7bf5
Lukas Winkler <l.winkler23@mailbox.org>
2017-07-03 21:43:48 +0200

    colored tags

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
 	<div id="app">
 		<div id="buttonwrapper">
-			<button class="button-outline" id="check1" @click="sort('name')">Sort by name</button>
+			<button class="button-outline" id="check1" @click="sort('title')">Sort by title</button>
 			<button class="button-outline" id="check2" @click="sort('id')">Sort by id</button>
+			<button class="button-outline" id="check3" @click="sort('date')">Sort by Date</button>
+			<button class="button-outline" id="shuffle" @click="shuffle()">Shuffle</button>
 			<br>
 			<button v-for="element in tags"
 					:class="['button-outline ',filters.includes(element)?'active':'']"
@@ -11,15 +13,20 @@
 			</button>
 			<button class="button-outline" @click="filters=[];filter('show all')">Reset</button>
 		</div>
-		<p>{{selected.name}} ({{selected.id}})</p>
+		<p>{{selected.title}} ({{selected.id}})</p>
 		<div id="blockwrapper">
 			<isotope ref="cpt" :list="data" id="root_isotope" class="isoDefault" :options='getOptions()'
 					 @filter="filterOption=arguments[0]">
-				<div v-for="element in data" @click="selected=element" :key="element.id" class="block">
-					{{element.name}}
-					<br> {{element.id}}
+				<div v-for="element in data" @click="selected=element" :key="element.title" class="block">
+					{{element.title}}
+					<br> {{element.niceDate}}
 					<br>
-					<span>{{element.tags.join(",")}}</span>
+					<div class="tagwrapper">
+						<div class="tag"
+							 v-for="tag in element.tags"
+							 :style="{backgroundColor: getColorByTag(tag)}">{{tag}}
+						</div>
+					</div>
 				</div>
 			</isotope>
 		</div>
@@ -29,14 +36,22 @@
 
 <script>
     import isotope from "vueisotope"
+    import * as colors from 'material-colors';
     import data from 'json-loader!yaml-loader!./data.yaml';
     export default {
         name: 'app',
         data () {
             return {
-                tags: ['a', 'b', 'c', 'g'],
+                tags: ['a', 'b', 'c', 'python'],
+                tagColors: {
+                    "a": "cyan",
+                    "b": "red",
+                    "c": "indigo",
+                    "python": "amber"
+                },
                 data: data,
                 selected: data[0],
+                ascending: true,
                 filters: [],
             }
         },
@@ -47,6 +62,9 @@
             filter: function(key) {
                 this.$refs.cpt.filter(key);
             },
+            shuffle: function(key) {
+                this.$refs.cpt.shuffle(key);
+            },
             tagfilter: function(element) {
                 if (this.filters.includes(element)) {
                     this.filters = this.filters.filter(item => item !== element)
@@ -55,13 +73,28 @@
                 }
                 this.filter('contains')
             },
+            getColorByTag(tag) {
+                let colorname = this.tagColors[tag];
+                if (colors[colorname]) {
+                    return colors[colorname]["500"];
+                }
+                return "black";
+            },
             getOptions: function() {
                 return {
                     getSortData: {
                         id: "id",
-                        name: "name"
+                        title: "title",
+                        date: function(elem) {
+                            if (elem.date) {
+                                return Date.parse(elem.date);
+                            }
+                            return Infinity;
+                        }
+
                     },
-                    sortBy: "name",
+                    sortAscending: this.ascending,
+                    sortBy: "title",
                     masonry: {
                         columnWidth: 70,
                         gutter: 10
@@ -69,13 +102,6 @@
                     getFilterData: {
                         "show all": function() {
                             return true;
-                        },
-                        number: function(el) {
-                            return el.id > 30;
-                        },
-                        "test": (el) => {
-                            console.log(el.tags.includes(this.filters[0]));
-                            return true
                         },
                         "contains": (element) => {
                             if (this.filters.length === 0) {
@@ -104,7 +130,7 @@
 	@import "../node_modules/milligram/src/milligram";
 
 	body {
-		background-color: #90CAF9;
+		/*background-color: #90CAF9;*/
 	}
 
 	#app {
@@ -117,7 +143,8 @@
 	}
 
 	.block {
-		background-color: white;
+		background-color: #009688;
+		color: white;
 		padding: 10px;
 		border-radius: 3px;
 		z-index: 3;
@@ -134,8 +161,24 @@
 			left: 0;
 			right: 0;
 			z-index: 0;
-			box-shadow: 5px 5px 17px 0 rgba(0, 0, 0, 0.2);
+			@include shadow(2);
+			/*box-shadow: 5px 5px 17px 0 rgba(0, 0, 0, 0.2);*/
 		}
+	}
+
+	.tag {
+		display: inline-block;
+		padding: .2em .6em .3em;
+		font-size: 75%;
+		font-weight: bold;
+		line-height: 1;
+		color: #ffffff;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: baseline;
+		border-radius: .25em;
+		margin-left: 2px;
+		margin-right: 2px;
 	}
 
 	button.active {
