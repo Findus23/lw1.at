33af2dc055dd0a7f2a0c9d87bb75ff1a2f3e2086
Lukas Winkler <l.winkler23@mailbox.org>
2018-05-23 10:10:05 +0200

    update privacy policy

--- src/Imprint.vue
+++ src/Imprint.vue
@@ -21,67 +21,229 @@
 			3500 Krems an der Donau<br>
 			Österreich
 		</address>
-		<!--
-				<h2>Urheberrecht und Haftung für Inhalte/Links</h2>
-				<p>
-					Die Inhalte der Seiten sind vom Autor der Webseite erstellt und unterliegen dem österreichischen
-					Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung bedürfen der
-					schriftlichen Zustimmung des Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten,
-					nicht kommerziellen Gebrauch gestattet bzw. bedürfen der Rücksprache.
-					Die Inhalte unserer Seiten wurden mit großer Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit
-					und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
-					Der Webauftritt enthält Links zu externen Webseiten, deren Inhalte wir nicht beeinflussen können.
-					Aus diesem Grund kann keinerlei Haftung für diese fremden Inhalte übernommen werden.
-				</p>
-		-->
-		<h2 v-if="language==='de'">Datenschutz</h2>
-		<h2 v-else>Privacy Policy</h2>
-		<p v-if="language==='de'">
-			Wie die meisten Webseiten sammelt lw1.at anonym Informationen, die der Webbrowser oder Server zur
-			Verfügung stellt, wie zum Beispiel die Art des Browsers, bevorzugte Sprachen, verweisende Webseite
-			und Zeitpunkt des Besuches. Diese Daten, durch welche die Besucher nicht eindeutig identifiziert
-			werden können, werden gesammelt, um besser zu verstehen, wie die Webseite benutzt wird. Hierzu wird
-			die Open-Source-Software <a href="https://piwik.org">Piwik</a> verwendet, wobei keine Daten an andere Firmen
-			oder Server
-			weitergegeben oder verkauft werden.
-		</p>
-		<p v-else>
-			Like most website operators, lw1.at collects non-personally-identifying information of the sort
-			that web browsers and servers typically make available, such as the browser type, language preference,
-			referring site, and the date and time of each visitor request. The purpose in collecting non-personally
-			identifying information is to better understand how the visitors use the website. We are using the
-			Open-Source-Software <a href="https://piwik.org">Piwik</a> in order that no data is forwarded or sold to
-			other servers or companies.
-		</p>
-		<p v-if="language==='de'">
-			Daten, welche Besucher eindeutig identifizieren könnten, werden ausschließlich in gekürzter Form
-			abgespeichert, wodurch sie nicht mehr eindeutig zugeordnet werden können. So werden von IP-Adressen
-			nur die ersten beiden Tupel (z. B. 194.118.xxx.xxx) gespeichert und verarbeitet. Wenn Sie verhindern
-			möchten, dass Daten über Sie gesammelt werden, können Sie es am Ende dieser Seite deaktivieren oder
-			<em><a href="http://donottrack.us/">Do-Not-Track</a></em> in Ihrem Browser aktivieren.
-		</p>
-		<p v-else>
-			Personally-identifying information like Internet Protocol (IP) addresses, are only collected in its
-			shortened form (e.g. 194.118.xxx.xxx), with the result that it is not possible to identify visitors
-			personally.
-			If want to disable this tracking, you can enable <a href="http://donottrack.us/">"Do-Not-Track"</a>
-			in your Browser or disable it below.
-		</p>
-		<p v-if="language==='de'">
-			Cookies sind kleine Textdateien, die vom Browser zwischenzeitlich gespeichert werden und bei jedem
-			Besuch an den Server gesendet werden. Sie dienen dazu den Besuch von einzelnen Unterseiten
-			nachvollziehen zu können und sich Einstellungen zwischen Besuchen zu merken Wenn Sie nicht möchten,
-			dass Cookies gespeichert werden, können Sie dies in Ihrem Browser deaktivieren.
-		</p>
-		<p v-else>
-			A cookie is a string of information that a website stores on a visitor’s computer, and that the visitor’s
-			browser provides to the website each time the visitor returns. We use cookies to help Piwik identify and
-			track visitors, their usage of the website, and their website access preferences. Visitors who do not wish
-			to have cookies placed on their computers should set their browsers to refuse cookies before using our
-			websites, with the drawback that certain features may not function properly without the aid of cookies.
-		</p>
-		<h2>Tracking Opt-Out</h2>
-		<iframe :src="'https://piwik.lw1.at/index.php?module=CoreAdminHome&amp;action=optOut&amp;idsite=14&amp;language='+language"></iframe>
+		<div v-if="language==='de'" class="mainText">
+			<h2>Datenschutzerklärung</h2>
+			<h3>Zweck der Datenverarbeitung</h3>
+			<p>Diese Webseite verwendet <a href="https://matomo.org/">Matomo</a>, eine Open Source, selbstgehostete
+				Software um
+				anonyme Nutzungsdaten für diese Webseite zu sammeln.</p>
+			<p>Die Daten zum Verhalten der Besucher werden gesammelt um eventuelle Probleme wie nicht gefundene Seiten,
+				Suchmaschinenprobleme oder unbeliebte Seiten herauszufinden. Sobald die Daten (Anzahl der Besucher die
+				Fehlerseiten oder nur eine Seite sehen, usw.) verarbeitet werden, erzeugt Matomo Berichte für die
+				Webseitenbetreiber, damit diese darauf reagieren können. (Layoutveränderungen, neue Inhalte, usw.)</p>
+			<h3>Matomo</h3>
+			<p>Matomo verarbeitet die folgenden Daten:</p>
+			<ul>
+				<li>Cookies</li>
+				<li>Anonymisierte IP-Adressen indem die letzten 2&nbsp;bytes entfernt werden (also 198.51.0.0&nbsp;anstatt
+					198.51.100.54)
+				</li>
+				<li>Pseudoanonymisierter Standort (basierend auf der anonymisierten IP-Adresse</li>
+				<li>Datum und Uhrzeit</li>
+				<li>Titel der aufgerufenen Seite</li>
+				<li>URL der aufgerufenen Seite</li>
+				<li>URL der vorhergehenden Seite (sofern diese das erlaubt)</li>
+				<li>Bildschirmauflösung</li>
+				<li>Lokale Zeit</li>
+				<li>Dateien die angeklickt und heruntergeladen wurden</li>
+				<li>Externe Links</li>
+				<li>Dauer des Seitenaufbaus</li>
+				<li>Land, Region, Stadt (mit niedriger Genauigkeit aufgrund von IP-Adresse)</li>
+				<li>Hauptsprache des Browsers</li>
+				<li>User Agent des Browsers</li>
+				<li>Interaktionen mit Formularen (aber nicht deren Inhalt)</li>
+			</ul>
+			<h3>Indirekte Datensammlung</h3>
+			<h4>Server Logs</h4>
+			<p>Wenn Sie diese Webseite verwenden, wird der Aufruf vom Host der Webseite (ich, Lukas Winkler)
+				aufgezeichnet. Dieses
+				Log enthält Ihre IP-Adresse, welche Sie indirekt über Ihren Internetanbieter identifiziert. Die
+				Aufzeichnung
+				dieser Daten ist gesetzlich verpflichtend und für die Sicherheit notwendig. Es gibt keine Möglichkeit
+				zum
+				Opt-Out, die Daten werden aber niemals für andere Zwecke verwendet.</p>
+			<h3>Basis des Legitimen Interesses</h3>
+			<p>Die Datenverarbeitung basiert auf dem Prinzip des legitimen Interesses.</p>
+			<p>Verarbeiten der Daten hilft uns herauszufinden, was auf unserer Seite funktioniert und was nicht. Zum
+				Beispiel
+				finden wir damit heraus, ob die Inhalte gut ankommen oder wie wir die Struktur der Webseite verbessern
+				können.
+				Unser Team profitiert davon und kann darauf reagieren. Aufgrund der Datenverarbeitung profitieren Sie
+				somit von
+				einer Webseite, die laufend besser wird.</p>
+			<p>Ohne den Daten könnten wir den Service nicht bieten. Ihre Daten werden ausschließlich zum Verbessern der
+				Webseitennutzung verwendet.</p>
+			<h3>Empfänger der Daten</h3>
+			<p>Die persönlichen Daten werden gesendet an:</p>
+			<ul>
+				<li>mich, Lukas Winkler (Host und Betreuer der Webseite und des Matomo Servers)</li>
+				<li>Hetzner Online GmbH (Host des Servers; <a href="https://www.hetzner.de/rechtliches/datenschutz/">Datenschutzerklärung</a>)
+				</li>
+			</ul>
+			<h3>Details zum Transfer in Drittstaaten</h3>
+			<p>Die Daten dieser Webseite und Matomo werden in Deutschland gehostet. Die Daten verlassen nie die EU.</p>
+			<h3>Rechte der betroffenen Personen</h3>
+			<p>Da Matomo Daten basierend auf legitimen Interesse sammelt, können Sie folgende Rechte ausüben:</p>
+			<ul>
+				<li><strong>Recht auf Auskunft und Datenübertragbarkeit</strong>: Sie können jederzeit alle Ihre Daten
+					anfordern.
+				</li>
+				<li><strong>Recht auf Löschung und Berichtigung</strong>: Sie können jederzeit anfragen, dass wir alle
+					Ihre
+					Daten vollständig löschen.
+				</li>
+				<li><strong>Recht auf Widerspruch und Einschränkung der Verarbeitung</strong>: Sie können jederzeit der
+					Datensammlung widersprechen, indem Sie in Ihrem Browser <a
+							href="https://www.eff.org/issues/do-not-track">DoNotTrack</a>
+					oder diese Box abhaken:
+				</li>
+			</ul>
+			<iframe src="https://piwik.lw1.at/index.php?module=CoreAdminHome&amp;action=optOut&amp;idsite=14&amp;language=de"></iframe>
+			<h3>Das Recht eine Beschwerde bei der Datenschutzbehörde einbringen</h3>
+			<p>Wenn Sie denken, dass die Art, wie wir Ihre Daten mit Matomo verarbeiten, gegen das Gesetz verstößt,
+				haben Sie
+				das Recht eine Beschwerde bei der Datenschutzbehörde einzubringen.</p>
+			<h2>Lizenz</h2>
+			<p>Diese Datenschutzerklärung basiert auf der Datenschutzerklärung des <a
+					href="https://matomo.org/privacy-policy/">Matomo
+				Projekts</a> und steht unter einer <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative
+				Commons Lizenz</a>. Sie können Sie also selbst verändern und weiterverwenden.</p>
+
+		</div>
+		<div v-else class="mainText">
+			<h2>Privacy Policy</h2>
+			<h3>Purpose of the processing</h3>
+			<p>This website is using <a href="https://matomo.org/">Matomo</a>, an Open Source, self-hosted software for
+				collecting anonymous usage statistics for this website.</p>
+			<p>The data is used to analyse the behaviour of the website visitors to identify potential pitfalls like not
+				found
+				pages, search engine indexing issues and to find out which contents are the most appreciated. Once the
+				data
+				(number of visitors reaching a not found pages, viewing only one page…) is processed, Matomo is
+				generating
+				reports for website owners to take action, for example changing the layout of the pages, publishing some
+				fresh
+				content… etc.</p>
+			<h3>Matomo</h3>
+			<p>Matomo is processing the following data:</p>
+			<ul>
+				<li>
+					Cookies
+				</li>
+				<li>
+					Anonymized IP-address by removing the last 2 bytes (so 198.51.0.0 instead of 198.51.100.54)
+				</li>
+				<li>
+					Pseudo-anonymized Location of the user (generated from the anonymized IP-address)
+				</li>
+				<li>
+					Date and time
+				</li>
+				<li>
+					Title of the page being viewed
+				</li>
+				<li>
+					URL of the page being viewed
+				</li>
+				<li>
+					URL of the page that was viewed prior to the current page (if the page allows it)
+				</li>
+				<li>
+					Screen resolution
+				</li>
+				<li>
+					Time in local timezone
+				</li>
+				<li>
+					Files that were clicked and downloaded
+				</li>
+				<li>
+					Link clicks to an outside domain
+				</li>
+				<li>
+					Pages generation time
+				</li>
+				<li>
+					Country, region, city (low resolution based on IP-address
+				</li>
+				<li>
+					Main Language of the browser
+				</li>
+				<li>
+					User Agent of the browser
+				</li>
+				<li>
+					Interactions with forms (but not the content)
+				</li>
+			</ul>
+			<h3>Indirect data collection</h3>
+			<h4>Server Logs</h4>
+			<p>If you are using this site, the visit is logged by the host of this website (myself, Lukas Winkler). This
+				log contains your IP-Address which allows you to be identified indirectly via your internet servrive
+				provider.
+				The collection of this data is a legal obligation and required for security. You cannot oppose it and
+				the data
+				is at no time used for other purposes.</p>
+
+			<h3>The legitimate interests</h3>
+			<p>The processing of personal data is based on legitimate interests.</p>
+			<p>Processing your personal data such as cookies is helping us identify what is working and what is not on
+				our
+				website. For example, it helps us identify if the way we are communicating is engaging or not and how we
+				can
+				organize the structure of the website better. Our team is benefiting from the processing of your
+				personal data,
+				and they are directly acting on the website. By processing your personal data, you can profit from a
+				website
+				which is getting better and better.</p>
+			<p>Without the data, we would not be able to provide you the service we are currently offering to you. Your
+				data
+				will be used only to improve the user experience on our website and help you find the information you
+				are
+				looking for.</p>
+
+			<h3>Recipient of the data</h3>
+			<p>The personal data received through Matomo are sent to:</p>
+			<ul>
+				<li>me, Lukas Winkler (host and maintainer of this website and Matomo instance)</li>
+				<li>
+					Hetzner Online GmbH (host of the server;
+					<a href="https://www.hetzner.de/rechtliches/datenschutz/">Privacy Policy</a>)
+				</li>
+			</ul>
+			<h3>Details of transfers to third country and safeguards</h3>
+			<p>Matomo and this website data is hosted in Germany. No data leaves the EU.</p>
+			<h3>Data subject’s rights</h3>
+			<p>As Matomo is processing personal data on legitimate interests, you can exercise the following rights:</p>
+			<ul>
+				<li>
+					<strong>Right of access and data portability</strong>: you can ask us at any time to access your
+					data.
+				</li>
+				<li>
+					<strong>Right to erasure and rectification</strong>: you can ask us at any time to delete all the
+					data we
+					are processing about you.
+				</li>
+				<li>
+					<strong>Right to object and restrict processing</strong>: you can object to the tracking of your
+					data by
+					using the following opt-out feature or by enabling <a
+						href="https://www.eff.org/issues/do-not-track">DoNotTrack</a>
+					in your browser:
+				</li>
+			</ul>
+			<iframe src="https://piwik.lw1.at/index.php?module=CoreAdminHome&amp;action=optOut&amp;idsite=14&amp;language=en"></iframe>
+			<h3>The right to lodge a complaint with a supervisory authority</h3>
+			<p>If you think that the way we process your personal data with Matomo analytics is infringing the law, you
+				have the
+				right to lodge a complaint with a supervisory authority.</p>
+			<h2>License</h2>
+			<p>This privacy policy is based on the privacy policy of <a href="https://matomo.org/privacy-policy/">the
+				Matomo
+				project</a> and licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative
+				Commons</a>, so you can modify it and use it yourself.</p>
+		</div>
 	</div>
 </template>
 
@@ -94,7 +256,7 @@
 
 <style lang="scss">
 	#imprint {
-		p, address {
+		.mainText {
 			text-align: left;
 		}
 		h2 {
