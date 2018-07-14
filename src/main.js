c4abe0cdb269dafb1346c4dacb9d6cf281ad70c6
Lukas Winkler <git@lw1.at>
2018-07-14 22:10:05 +0200

    fix js and move it into index

--- src/main.js
+++ src/main.js
@@ -10,10 +10,6 @@ let matomo = new MatomoTracker;
 matomo.init();
 
 if ((typeof [].find) !== "undefined") { // if the browser doesn't support modern IE 
-    var noscript_el = document.getElementById("noscript");
-    if (noscript_el) {
-        noscript_el.style.display = "none";
-    }
     let app = new Vue({
         el: '#app',
         router,
