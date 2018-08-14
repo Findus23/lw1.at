c8c4a58ed2ae58e0a4ee3663bd4b9f3a868ae73c
Lukas Winkler <git@lw1.at>
2018-08-14 21:44:51 +0200

    fix brackets

--- src/MatomoTracker.js
+++ src/MatomoTracker.js
@@ -10,13 +10,13 @@ export default class MatomoTracker {
             }
             (function() {
                 let u = (process.env.NODE_ENV === "production") ? "https://matomo.lw1.at/" : "//localhost/piwik/";
-                _paq.push(['setTrackerUrl', u + (process.env.NODE_ENV === "production") ? 'statistics.php' : 'piwik.php']);
+                _paq.push(['setTrackerUrl', u + ((process.env.NODE_ENV === "production") ? 'statistics.php' : 'piwik.php')]);
                 _paq.push(['setSiteId', (process.env.NODE_ENV === "production") ? 14 : 5]);
                 let d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
                 g.type = 'text/javascript';
                 g.async = true;
                 g.defer = true;
-                g.src = u + (process.env.NODE_ENV === "production") ? 'statistics.js' : 'piwik.js';
+                g.src = u + ((process.env.NODE_ENV === "production") ? 'statistics.js' : 'piwik.js');
                 s.parentNode.insertBefore(g, s);
             })();
             window._paq = _paq;
