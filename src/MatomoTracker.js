d2c2cbde86c7e918be0af3891f978b0626abefb8
Lukas Winkler <git@lw1.at>
2018-08-14 21:41:58 +0200

    rename Matomo

--- src/MatomoTracker.js
+++ src/MatomoTracker.js
@@ -2,6 +2,7 @@ export default class MatomoTracker {
     init() {
         if (typeof _paq === 'undefined' && !window.__PRERENDER_INJECTED) { // should only occur with hot reloading
             let _paq = _paq || [];
+            _paq.push(['setRequestMethod', 'POST']);
             _paq.push(['enableHeartBeatTimer']);
             _paq.push(['HeatmapSessionRecording::disableAutoDetectNewPageView']);
             if (process.env.NODE_ENV === "production") {
@@ -9,13 +10,13 @@ export default class MatomoTracker {
             }
             (function() {
                 let u = (process.env.NODE_ENV === "production") ? "https://matomo.lw1.at/" : "//localhost/piwik/";
-                _paq.push(['setTrackerUrl', u + 'piwik.php']);
+                _paq.push(['setTrackerUrl', u + (process.env.NODE_ENV === "production") ? 'statistics.php' : 'piwik.php']);
                 _paq.push(['setSiteId', (process.env.NODE_ENV === "production") ? 14 : 5]);
                 let d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
                 g.type = 'text/javascript';
                 g.async = true;
                 g.defer = true;
-                g.src = u + 'piwik.js';
+                g.src = u + (process.env.NODE_ENV === "production") ? 'statistics.js' : 'piwik.js';
                 s.parentNode.insertBefore(g, s);
             })();
             window._paq = _paq;
