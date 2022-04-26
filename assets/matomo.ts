b4a6d1f374780fa8b8fc6369bd556ca6b99ed0b7
Lukas Winkler <git@lw1.at>
2022-04-26 15:25:56 +0200

    use MatomoLiteTracker

--- assets/matomo.ts
+++ assets/matomo.ts
@@ -1,37 +1,21 @@
 import {isDebug} from "./utils"
-
-declare global {
-    interface Window {
-        _paq: paq
-        __PRERENDER_INJECTED: boolean
-    }
-}
-
-declare type paq = [[string, unknown, unknown, unknown] | [string, unknown, unknown] | [string, unknown] | [string]]
+import {MatomoLiteTracker} from "matomo-lite-tracker/src/tracker"
+import {PerformanceMetric} from "matomo-lite-tracker/src/performancetracking"
+import {BrowserFeatures} from "matomo-lite-tracker/src/browserfeatures"
+import {enableLinkTracking} from "matomo-lite-tracker/src/linktracking"
+import {isDoNotTrackEnabled} from "matomo-lite-tracker/src/util"
 
 
 export function initMatomo(): void {
-    if (isDebug) {
+    if (isDebug|| isDoNotTrackEnabled()) {
         return
     }
-    const _paq = window._paq || [] as unknown as paq
-    _paq.push(['setRequestMethod', 'POST'])
-    _paq.push(['enableHeartBeatTimer'])
+    const matomo = new MatomoLiteTracker("https://matomo.lw1.at", 14)
+    matomo.performanceMetric = new PerformanceMetric()
 
-    _paq.push(["setDoNotTrack", true])
-    _paq.push(['disableCookies'])
-    const matomoURL = "https://matomo.lw1.at/statistics"
-    _paq.push(['setTrackerUrl', matomoURL + ".php"])
-    _paq.push(['setSiteId', 14])
-    const script = document.createElement('script')
-    const firstScript = document.getElementsByTagName('script')[0]
-    script.type = 'text/javascript'
-    script.async = true
-    script.defer = true
-    script.src = matomoURL + ".js"
-    if (firstScript.parentNode) {
-        firstScript.parentNode.insertBefore(script, firstScript)
-    }
-    window._paq = _paq
+    matomo.browserFeatures = new BrowserFeatures()
+
+    enableLinkTracking(matomo, [])
+    matomo.trackPageview()
 }
 
