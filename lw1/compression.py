c16e40274fa20c6a71d5c5a0edef5c95688207ea
Lukas Winkler <git@lw1.at>
2022-05-15 23:07:39 +0200

    add brotli compression

--- lw1/compression.py
+++ lw1/compression.py
@@ -7,6 +7,7 @@ def compress_files():
     for file in output_dir.glob("**/*"):
         if file.is_dir():
             continue
-        if file.suffix not in {".html", ".css", ".js", ".xml", ".map", ".json", ".woff", ".woff2", ".ttf"}:
+        if file.suffix not in {".html", ".css", ".js", ".xml", ".map", ".json", ".woff", ".woff2", ".ttf", ".svg"}:
             continue
         run(["gzip", "-k", "-f", "--best", str(file)], check=True)
+        run(["brotli", "-f", "--best", str(file)], check=True)
